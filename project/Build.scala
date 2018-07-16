dab8b0b78276cf36d7205b34a4b82bbd52e587cf
Daniel James <dan@gemini.com>
2018-07-16 18:11:21 -0400

    Customize publish with envvars

--- project/Build.scala
+++ project/Build.scala
@@ -118,9 +118,10 @@ object SlickBuild extends Build {
     logBuffered := false,
     repoKind := (if (version.value.trim.endsWith("SNAPSHOT")) "snapshots" else "releases"),
     publishTo := (repoKind.value match {
-      case "snapshots" => Some("snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
-      case "releases" =>  Some("releases"  at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
+      case "snapshots" => Some("snapshots" at s"https://${sys.env("ARTIFACTORY_HOST")}/artifactory/ext-snapshot-local;build.timestamp=" + new java.util.Date().getTime) // Some("snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
+      case "releases" =>  Some("releases" at s"https://${sys.env("ARTIFACTORY_HOST")}/artifactory/ext-release-local") // Some("releases"  at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
     }),
+    credentials += Credentials("Artifactory Realm", sys.env("ARTIFACTORY_HOST"), sys.env("ARTIFACTORY_USER"), sys.env("ARTIFACTORY_PASSWORD")),
     publishMavenStyle := true,
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => false },
